90cfe9b041840ee968f33a8e0983e4541da81a4d
lvsantosdev <ldsantos.developer@gmail.com>
2025-06-14 17:04:12 -0300

    adicionado base do codigo

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import calculadora
+
+a = 10
+b = 5
+print(f"{a} + {b} = {calculadora.soma(a, b)}")
+print(f"{a} - {b} = {calculadora.sub(a, b)}")
+print(f"{a} * {b} = {calculadora.mult(a, b)}")
+print(f"{a} / {b} = {calculadora.div(a, b)}")
